83c69d3bcc83b9695050d90bb4430a2e3c4a1e03
Paul Adams <paul@adams.io>
2021-05-03 09:10:04 -0500

    Update main.js
    * Disabled navPanel

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -40,7 +40,7 @@
 		// Title Bar.
 			$(
 				'<div id="titleBar">' +
-					'<a href="#navPanel" class="toggle"></a>' +
+					// '<a href="#navPanel" class="toggle"></a>' +
 					'<span class="title">' + $('#logo').html() + '</span>' +
 				'</div>'
 			)
